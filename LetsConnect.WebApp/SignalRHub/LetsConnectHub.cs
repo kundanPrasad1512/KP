184f4746ffbb7a3624f216ff21fde7e2b31bdf0c
Kundan Prasad <kundanprasad1512@gmail.com>
2020-02-22 00:26:42 +0530

    initial commit 2

---
+++ LetsConnect.WebApp/SignalRHub/LetsConnectHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsConnect.WebApp.SignalRHub.Hubs
+{
+    public class LetsConnectHub:Hub
+    {
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        //public Task SendToAll(string name, string message)
+        //{
+        //    return Clients.All.SendAsync("ReceiveMessage", name, message);
+        //}
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.SendAsync("sendToAll", name, message);
+        }
+
+        //public string GetConnectionId()
+        //{
+        //    return Context.ConnectionId;
+        //}
+
+        //public override async Task OnDisconnectedAsync(Exception ex)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, "left");
+        //}
+
+        public async Task ReceiveMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
